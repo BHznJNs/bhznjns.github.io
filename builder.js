d6b9fcd4fcce3a0ee29261023069b5e1c1fc7e02
BHznJNs <441768875@qq.com>
2023-07-07 10:51:28 +0800

    comments added

--- builder.js
+++ builder.js
@@ -52,15 +52,19 @@ function indexing(currentDir, currentName) {
 
     for (const item of currentDir) {
         if (typeof item == "string") {
+            // directly push filename
             currentDirFiles.push(item)
         } else {
+            // push folder name with '/' at end
             currentDirFiles.push(item[0] + "/")
+            // recursively read folder
             indexing(item[1], `${currentName}+${item[0]}`)
         }
     }
 
     const sliced = slicing(currentDirFiles)
     const count = sliced.length
+
     let index = 0
     for (const slice of sliced) {
         index += 1
