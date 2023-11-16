74c2d373567e22f4a342167cc63348ce7b4033e5
BHznJNs <441768875@qq.com>
2023-11-16 09:56:20 +0800

    sort blogs by creating time

--- builder.js
+++ builder.js
@@ -11,7 +11,7 @@ function readDir(path) {
 
     // sort by time
     dirContent.sort((a, b) =>
-        fs.statSync(path + b).mtime.getTime() - fs.statSync(path + a).mtime.getTime())
+        fs.statSync(path + b).ctime.getTime() - fs.statSync(path + a).ctime.getTime())
 
     for (const item of dirContent) {
         const currentPath = path + item
