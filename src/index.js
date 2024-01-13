bc5c02dedfe234791e2f9773ea2bc081bed46073
BHznJNs <441768875@qq.com>
2024-01-13 10:43:39 +0800

    formula style updated & page changing bug fixed

--- src/index.js
+++ src/index.js
@@ -2,33 +2,13 @@ import "./styles/style.css"
 import "./libs/katex/katex.css"
 
 import "./scripts/mainManager.js"
-import { hashChangeEvent } from "./scripts/pathManager.js"
 import pageManager from "./scripts/pageManager.js"
 import keydownEvent from "./utils/keydownEvent.js"
 
-// ---------------------------
-// Buttons event setting start
-// ---------------------------
 const lightBtn = document.querySelector("#light-btn")
 const darkBtn = document.querySelector("#dark-btn")
-const previousBtn = document.querySelector("button#previous")
-const nextBtn = document.querySelector("button#next")
-
 lightBtn.onkeydown = keydownEvent(lightBtn)
 darkBtn.onkeydown = keydownEvent(darkBtn)
-previousBtn.addEventListener("click", () => {
-    if (pageManager.current() > 0) {
-        pageManager.previous()
-        hashChangeEvent()
-    }
-})
-nextBtn.addEventListener("click", () => {
-    pageManager.next()
-    hashChangeEvent()
-})
-// -------------------------
-// Buttons event setting end
-// -------------------------
 
 window.addEventListener("popstate", () => {
     if (location.hash.endsWith("/")) {
