3bc7e05754abf9a8227958f198adeec197698bf4
BHznJNs <441768875@qq.com>
2024-12-29 22:02:39 +0800

    sync with template & Follow rss header

--- build.config.js
+++ build.config.js
@@ -62,22 +62,22 @@ export default {
 
     /**
      * enableRSS: Boolean
-     * used to enable / disable the RSS function, if you do not want add blogs in some directory to the `rss.xml` , just add it into the `rssIgnoreDir`
-     * 用来启用/禁用 RSS 发布功能，如果你不想将某些文件夹的内容添加到 `rss.xml` 中，将其添加到 `rssIgnoreDir` 中
+     * used to enable / disable the RSS function, if you do not want add blogs in some directory to the `rss.xml` , just add it into the `RSSIgnoredDir`
+     * 用来启用/禁用 RSS 发布功能，如果你不想将某些文件夹的内容添加到 `rss.xml` 中，将其添加到 `RSSIgnoredDir` 中
      */
     enableRSS: true,
-    rssIgnoreDir: [
+    RSSIgnoredDir: [
         // directories in `static`
         "学习笔记/"
     ],
 
     /**
      * enableNewest: Boolean
-     * used to enable / disable the Newest function, if you do not want add blogs in some directory, just add it into the `newestIgnoreDir`
-     * 用来启用/禁用 最新博文 功能，如果你不想将某些文件夹的内容添加到其中，将其添加到 `newestIgnoreDir` 中
+     * used to enable / disable the Newest function, if you do not want add blogs in some directory, just add it into the `newestIgnoredDir`
+     * 用来启用/禁用 最新博文 功能，如果你不想将某些文件夹的内容添加到其中，将其添加到 `newestIgnoredDir` 中
      */
     enableNewest: true,
-    newestIgnoreDir: [],
+    newestIgnoredDir: [],
 
     /**
      * enableSearch: Boolean
@@ -106,6 +106,17 @@ export default {
      * 定义 `rss.xml` 中包含的博文个数
      */
     RSSCapacity: 50,
+
+    /**
+     * RSSExtraHeader: String
+     * defined the extra headers in `rss.xml`
+     * 定义 `rss.xml` 中包含的额外头数据
+     */
+    RSSExtraHeader: `\
+<follow_challenge>
+    <feedId>96181789796736000</feedId>
+    <userId>96180252670732288</userId>
+</follow_challenge>`,
 
     /**
      * previewPort: Number
