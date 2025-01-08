449bbff93d1d589e8daef621cd850d61eb6698f0
BHznJNs <441768875@qq.com>
2025-01-08 21:06:36 +0800

    analysis script updated

--- user/external.js
+++ user/external.js
@@ -2,4 +2,8 @@ window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 
-gtag('config', 'G-NV45LQLRQW');
+gtag('config', 'G-NV45LQLRQW', {
+    // prevent cross-site issues
+    cookie_domain: 'none',
+    cookie_flags: 'SameSite=None; Secure',
+});
