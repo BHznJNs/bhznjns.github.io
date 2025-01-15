4c0868a40cdf9c418bd40dfeaf4414bb9d976f4c
BHznJNs <441768875@qq.com>
2025-01-15 20:54:46 +0800

    removed deep clone

--- builder/utils/slice.js
+++ builder/utils/slice.js
@@ -5,25 +5,23 @@ import { config } from "./loadConfig.js"
 //                             ||
 //                             \/
 // [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]]
-// 
-// returns deep clone of input iterator
 const { pageSize } = config
 export default function(itemList) {
     let startIndex = 0
     let remainItemCount = itemList.length - (startIndex + 1)
     if (remainItemCount <= pageSize) {
-        return [ structuredClone(itemList) ]
+        return [ itemList ]
     } else {
         const slices = []
         while (remainItemCount > pageSize) {
             const listSlice = itemList.slice(startIndex, startIndex + pageSize)
-            slices.push(structuredClone(listSlice))
+            slices.push(listSlice)
             remainItemCount -= pageSize
             startIndex += pageSize
         }
         if (itemList.length) {
             const listSlice = itemList.slice(startIndex, startIndex + pageSize)
-            slices.push(structuredClone(listSlice))
+            slices.push(listSlice)
         }
         return slices
     }
